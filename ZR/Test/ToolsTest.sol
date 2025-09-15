7e50a64f5307c460eed5096216e260ecce5c8d5b
favesing <faves.007@163.com>
2025-09-16 00:00:21 +0800

    55

---
+++ ZR/Test/ToolsTest.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract ToolsTest {
+    
+    function hashSelector(string memory msign) external pure returns(bytes32){
+        return keccak256(bytes(msign));
+    }
+
+    function byte4Selector(string memory msign) external pure returns(bytes4){
+        return bytes4(keccak256(bytes(msign)));
+    }
+
+    function encodeAbi(bytes4 msign) external pure returns(bytes memory){
+        return abi.encodeWithSelector(msign);
+    }
+
+    function encodeAbi(bytes4 msign, address addr, uint256 amount) external pure returns(bytes memory){
+        return abi.encodeWithSelector(msign, addr, amount);
+    }
+}
