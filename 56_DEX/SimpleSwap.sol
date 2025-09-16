cae3a5f773fe0ce3b31bb72fdad27ade3e3c9280
favesing <faves.007@163.com>
2025-09-16 12:21:59 +0800

    57 - end

--- 56_DEX/SimpleSwap.sol
+++ 56_DEX/SimpleSwap.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract TestERC20 is ERC20{
+    constructor(string memory name, string memory symbol) ERC20(name, symbol){
+        _mint(_msgSender(), 100 ether);
+    }    
+}
+
 contract SimpleSwap is ERC20 {
     // 代币合约
     IERC20 public token0;
