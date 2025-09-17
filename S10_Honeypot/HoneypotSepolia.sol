9bb49c06b42a47a168758dbd696f5c25f5ce031f
favesing <faves.007@163.com>
2025-09-17 12:46:45 +0800

    s10

--- S10_Honeypot/HoneypotSepolia.sol
+++ S10_Honeypot/HoneypotSepolia.sol
@@ -3,23 +3,24 @@ pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
- 
+
 // 极简貔貅ERC20代币，只能买，不能卖
 contract HoneyPotSepolia is ERC20, Ownable {
     address public pair;
     // 构造函数：初始化代币名称和代号
-    constructor() ERC20("PXS1", "PXS1") Ownable(msg.sender){
-        address factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f; 
-        address tokenA = address(this);
+    constructor() ERC20("PXS", "PXS") Ownable(msg.sender){
+        address factory = 0xB7f907f7A9eBC822a80BD25E224be42Ce0A698A0; 
+        address tokenA = 0xd6a9cec6c2E8a71E556F721776e158e1Bb9eF5a9;
         address tokenB = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14; 
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA); //将tokenA和tokenB按大小排序
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        bytes32 initCodeHash = 0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303;
         // calculate pair address
         pair = address(uint160(uint(keccak256(abi.encodePacked(
-        hex'ff',
-        factory,
-        salt,
-        hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+          hex"ff",
+          factory,
+          salt,
+          initCodeHash
         )))));
     }
     
@@ -38,7 +39,7 @@ contract HoneyPotSepolia is ERC20, Ownable {
       address from,
       address to,
       uint256 amount
-  ) internal virtual override {
+  ) internal virtual override  {
      if(to == pair){
         require(from == owner(), "Can not Transfer");
       }
